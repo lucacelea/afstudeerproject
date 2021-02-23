d0868b29aaa4d74a9e2827300b264499223b8dc6
Rafael Polfliet <rafael.polfliet@student.ucll.be>
2021-02-23 16:58:56 +0100

    logboek

--- people-counting-opencv/timer.py
+++
@@ -1,6 +0,0 @@
-import datetime, time
-start = datetime.timedelta(seconds=0)
-while start <= datetime.timedelta(seconds=10):
-    print(start)
-    start += datetime.timedelta(seconds=1)
-    time.sleep(1)
